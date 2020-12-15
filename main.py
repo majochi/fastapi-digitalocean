cededdc5efcee5cc7d3b466698919d4dcf6a3be0
Joachim Knust <jo@goetheborg.net>
2020-12-15 14:02:57 +0100

    typecast port to int

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ if __name__ == '__main__':
     import uvicorn
     import os
 
-    uvicorn.run("main:app", host="0.0.0.0", port=os.environ.get("PORT", 8000), log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), log_level="info")
