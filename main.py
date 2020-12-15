a28109fe9112a9905ca46771e2b9373d9a650804
Joachim Knust <jo@goetheborg.net>
2020-12-15 13:49:52 +0100

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def index():
+    return {"message": "Hello World!"}
+
+
+@app.get("/help")
+async def help():
+    return {"message": "No help available."}
+
+
+if __name__ == '__main__':
+    import uvicorn
+    import os
+
+    uvicorn.run("main:app", host="0.0.0.0", port=os.environ.get("PORT", 8000), log_level="info")
